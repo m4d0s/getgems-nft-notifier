054d27c7314ef13d997909fb0581d6c12c5b2f8c
Smile Greenlight <60644076+M4dOS@users.noreply.github.com>
2024-09-02 23:07:18 +1000

    .sh file

---
+++ start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Обновляем pip
+python3 -m pip install --upgrade pip
+
+# Устанавливаем зависимости из файла req.txt
+pip3 install -r req.txt
+
+# Запускаем telegram.py в фоне
+python3 telegram.py
+
